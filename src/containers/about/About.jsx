1fbb5ad237a87f126846c9f9fd7e47b59d891089
Abraham vidal <antonio.vidal95@hotmail.com>
2020-07-17 02:55:02 -0400

    animacion capas primera y segunda en about

--- src/containers/about/About.jsx
+++ src/containers/about/About.jsx
@@ -2,7 +2,10 @@ import React, { useEffect, Fragment, useState } from "react";
 
 import universe from "../../resources/img/planets2.png";
 import notSky from "../../resources/img/notSky.png";
+import mountaintop from "../../resources/img/mountaintop.png"
 import bottomMountain from "../../resources/img/mountainsBottom.png";
+import primera from "../../resources/img/primera.png"
+import segunda from "../../resources/img/segunda.png"
 import "./About.css";
 import { Stars, Card, Contact } from "./components";
 
@@ -17,10 +20,19 @@ const About = (props) => {
       </div>
       <Stars />
       <div>
-      <img src={notSky}  style={{  zIndex: 1500 }} alt="house" className="house" />
+      <img src={primera}  style={{  zIndex: 1100 }} alt="primera" className="primera" />
       </div>
       <Card card={card}/>
       <Contact contact={contact}/>
+       <div>
+        <img
+          src={segunda}
+          alt="segunda"
+          style={{  zIndex: 1000 }}
+          className="segunda"
+          id="mountain-bottom"
+        />
+      </div> 
       {/* <div>
         <img
           src={bottomMountain}
@@ -29,7 +41,7 @@ const About = (props) => {
           className="bottomMountain"
           id="mountain-bottom"
         />
-      </div> */}
+      </div>  */}
     </div>
   );
 };
